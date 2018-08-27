b70a1f2dde9edfda43c294f2cd16019f4b79bfb9
kiran namani <kiran.mullinsadvisory@gmail.com>
2018-08-28 06:35:11 +1000

    Json Parsing project

---
+++ JsonParser/Models/PetOwner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonParser.Models
+{
+  public class PetOwner
+  {
+    public string name { get; set; }
+    public string gender { get; set; }
+    public int age { get; set; }
+    public List<Pet> pets { get; set; }
+  }
+
+}
---
+++ JsonParser/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using JsonParser.Models;
+using Newtonsoft.Json;
+
+namespace JsonParser
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data\Pet.json");
+
+      var petInfoAllText = System.IO.File.ReadAllText(path);
+
+      var petOwners = JsonConvert.DeserializeObject<List<PetOwner>>(petInfoAllText);
+
+     // print male owner pet Cat names
+      Console.WriteLine("Male");
+      PrintPetNames(petOwners, "Male","Cat");
+
+      // print female owner pet Cat names
+      Console.WriteLine("Female");
+      PrintPetNames(petOwners, "Female","Cat");
+    }
+
+    private static void PrintPetNames(IEnumerable<PetOwner> petOwners, string gender, string petType)
+    {
+            var owners = petOwners.Where(po => po.gender == gender).ToList();
+            List<Pet> pets = new List<Pet>();
+            foreach (var petOwner in owners)
+            {
+                List<Pet> tempPet = petOwner.pets?.Where(p => p.type == petType).ToList();
+                if (tempPet != null)
+                    pets.AddRange(tempPet);
+            }
+
+            pets = pets.OrderBy(pet => pet.name).ToList();
+            foreach (var pet in pets)
+            {
+                Console.WriteLine($".   {pet.name}");
+            }
+    }
+  }
+}
